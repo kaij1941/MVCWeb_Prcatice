9835db4a96dd600396d7d527d1b038fa2d376ab9
Jacky_Cao <kaij1941@gmail.com>
2020-07-12 22:33:51 +0800

    會員列表及初始頁面設定

---
+++ MVCWebSite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MVCWebSite.Models;
+namespace MVCWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbMemberEntities db = new dbMemberEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var emps = db.tMember.OrderBy(m => m.Mem_Id).ToList();
+
+            return View(emps);
+        }
+    }
+}
