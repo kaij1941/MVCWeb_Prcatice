585ae11a8fcd2b8838d2e77fc21396f2d7bf788d
Jacky_Cao <kaij1941@gmail.com>
2020-07-12 23:56:03 +0800

    會員修改/刪除

--- MVCWebSite/Controllers/HomeController.cs
+++ MVCWebSite/Controllers/HomeController.cs
@@ -33,5 +33,30 @@ namespace MVCWebSite.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Edit(int Mem_Id) 
+        {
+            var member = db.tMember.Where(m => m.Mem_Id == Mem_Id).FirstOrDefault();
+            return View(member);
+        }
+        [HttpPost]
+        public ActionResult Edit(int Mem_Id, string Mem_Name, string Mem_Phone, int Mem_salary)
+        {
+            var member = db.tMember.Where(m => m.Mem_Id == Mem_Id).FirstOrDefault();
+            member.Mem_Name = Mem_Name;
+            member.Mem_Phone = Mem_Phone;
+            member.Mem_Salary = Mem_salary;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int Mem_Id)
+        {
+            //var member = db.tMember.Where(m => m.Mem_Id == Mem_Id).FirstOrDefault();
+            //db.tMember.Remove(member);
+            //db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
