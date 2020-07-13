f909968f7e2df62df751158b393fef1ba0ed266b
Jacky_Cao <kaij1941@gmail.com>
2020-07-13 23:17:44 +0800

    改善讀取頁面，增加搜尋功能

--- MVCWebSite/Controllers/HomeController.cs
+++ MVCWebSite/Controllers/HomeController.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using LinqKit;
 using MVCWebSite.Models;
+using MVCWebSite.Service;
 namespace MVCWebSite.Controllers
 {
     public class HomeController : Controller
     {
         dbMemberEntities db = new dbMemberEntities();
-        // GET: Home
+
+        //[ActionName("路由名稱")] //可用此方式於網址路由中不直接用Action Function的名稱
+        //[HttpGet]預設為Get傳送//可省略
         public ActionResult Index()
         {
-            var emps = db.tMember.Where(m=>m.Mem_Exist =="0").OrderBy(m => m.Mem_Id).ToList();
-
-            return View(emps);
+            var member = db.tMember.Where(m=>m.Mem_Exist =="0").OrderBy(m => m.Mem_Id).ToList();
+            return View(member);
         }
-
+        [HttpPost]
+        public ActionResult Index(int Mem_Id =0, string Mem_Name="", string Mem_Phone="", int Mem_salaryMax=100000, int Mem_salaryMin=0) 
+        {
+            var filter = PredicateBuilder.New<tMember>();
+            filter.And(a => a.Mem_Exist == "0");
+            if (Mem_Id != 0)
+            { filter = filter.And(a => a.Mem_Id == Mem_Id); }
+            if (Mem_Name.Trim() != "")
+            { filter = filter.And(a => a.Mem_Name == Mem_Name); }
+            if (Mem_Phone.Trim() != "")
+            { filter = filter.And(a => a.Mem_Phone == Mem_Phone); }
+            if (Mem_salaryMax != 100000)
+            { filter = filter.And(a => a.Mem_Salary <= Mem_salaryMax); }
+            if (Mem_salaryMin != 0)
+            { filter = filter.And(a => a.Mem_Salary >= Mem_salaryMin); }
+            var member = db.tMember.Where(filter) .OrderBy(m => m.Mem_Id).ToList();
+            return View(member);
+        }
         public ActionResult Create() 
         {
             return View();
@@ -51,8 +70,10 @@ namespace MVCWebSite.Controllers
         }
         public ActionResult Delete(int Mem_Id)
         {
+            //以直接delete實作刪除功能
             //var member = db.tMember.Where(m => m.Mem_Id == Mem_Id).FirstOrDefault();
             //db.tMember.Remove(member);
+            //以update實作刪除功能
             var member = db.tMember.Where(m => m.Mem_Id == Mem_Id).FirstOrDefault();
             member.Mem_Exist = "1";
             db.SaveChanges();
